849eba82dfbdd30c85b1afc4163731209723833b
sandipk01 <sandipkengar01@gmail.com>
2020-04-10 19:30:19 +0530

    ADDED : Test case for checking feet to inches

--- src/test/java/com/bridgelabz/test/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/test/QuantityMeasurementTest.java
@@ -1,9 +1,21 @@
 package com.bridgelabz.test;
 
+import com.bridgelabz.service.UnitConverter;
+import com.bridgelabz.service.QuantityMeasurement;
+import com.bridgelabz.service.Units;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class QunatityMeasurementTest {
+public class QuantityMeasurementTest {
+
+    private QuantityMeasurement quantityMeasurement;
+
     @Test
-    public void GivenFeetValue_WhenCompareToInches_ThenShouldReturnTrue() {
+    public void givenFeetAndInches_WhenCompareFeetToInches_ThenShouldReturnTrue() {
+        UnitConverter converterInches = new UnitConverter(1, Units.FEET);
+        UnitConverter converterFeet = new UnitConverter(12, Units.INCHES);
+        quantityMeasurement = new QuantityMeasurement();
+        boolean inchAndFeet = quantityMeasurement.compare(converterInches, converterFeet);
+        Assert.assertEquals(true, inchAndFeet);
     }
 }
