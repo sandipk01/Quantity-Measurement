0481ce81e6f5e72dd5920b91c7655d2c8b634b8c
sandipk01 <sandipkengar01@gmail.com>
2020-04-10 20:35:44 +0530

    REFACTORED : Unit type variable added.

--- src/main/java/com/bridgelabz/service/UnitConverter.java
+++ src/main/java/com/bridgelabz/service/UnitConverter.java
@@ -3,10 +3,16 @@ package com.bridgelabz.service;
 public class UnitConverter {
 
     private double value;
+    private UnitType unitType;
 
     //Parameterized constructor for passing argument value and unit.
     public UnitConverter(double value, Units unit) {
         this.value=Converting(value,unit);
+        this.unitType=unit.getUnitType();
+    }
+
+    public UnitType getUnitType() {
+        return unitType;
     }
 
     //Method to Convert value into particular Unit.
