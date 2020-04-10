8d8d2a45aa49b764e3952faa490bed62c21aa4db
sandipk01 <sandipkengar01@gmail.com>
2020-04-10 20:15:16 +0530

    ADDED : Given same object then should return return

--- src/test/java/com/bridgelabz/test/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/test/QuantityMeasurementTest.java
@@ -10,14 +10,15 @@ import org.junit.Test;
 public class QuantityMeasurementTest {
 
     private QuantityMeasurement quantityMeasurement;
+    private boolean result;
 
     @Test
     public void givenFeetAndInches_WhenCompareFeetToInches_ThenShouldReturnTrue() throws QuantityMeasurementException {
         UnitConverter converterInches = new UnitConverter(1, Units.FEET);
         UnitConverter converterFeet = new UnitConverter(12, Units.INCHES);
         quantityMeasurement = new QuantityMeasurement();
-        boolean inchAndFeet = quantityMeasurement.compare(converterInches, converterFeet);
-        Assert.assertEquals(true, inchAndFeet);
+        result = quantityMeasurement.compare(converterInches, converterFeet);
+        Assert.assertEquals(true, result);
     }
 
     @Test
@@ -25,19 +26,27 @@ public class QuantityMeasurementTest {
         UnitConverter converterInches = new UnitConverter(0, Units.FEET);
         UnitConverter converterFeet = new UnitConverter(0, Units.FEET);
         quantityMeasurement = new QuantityMeasurement();
-        boolean inchAndFeet = quantityMeasurement.compare(converterInches, converterFeet);
-        Assert.assertEquals(true, inchAndFeet);
+        result = quantityMeasurement.compare(converterInches, converterFeet);
+        Assert.assertEquals(true, result);
     }
 
     @Test
     public void givenFeetObjectNull_WhenCompare_ThenShouldThrowsNullException(){
-        UnitConverter converterInches = new UnitConverter(0, Units.FEET);
+        UnitConverter converterFeet = new UnitConverter(0, Units.FEET);
         quantityMeasurement = new QuantityMeasurement();
         try {
-            boolean inchAndFeet = quantityMeasurement.compare(converterInches, null);
+            result = quantityMeasurement.compare(converterFeet, null);
         } catch (QuantityMeasurementException e) {
             Assert.assertEquals(QuantityMeasurementException.ExceptionType.NULL_EXCEPTION,e.type);
         }
+    }
+
+    @Test
+    public void givenSameObject_WhenCompare_ThenShouldReturnTrue() throws QuantityMeasurementException {
+        UnitConverter converterFeet = new UnitConverter(0, Units.FEET);
+        quantityMeasurement = new QuantityMeasurement();
+        result = quantityMeasurement.compare(converterFeet, converterFeet);
+        Assert.assertEquals(true, result);
     }
 
 }
