c53da3b1eecb8fffa2b7235857133b30e0968c86
sandipk01 <sandipkengar01@gmail.com>
2020-04-10 20:03:49 +0530

    REFACTORED : Null exception is throw

--- src/main/java/com/bridgelabz/service/QuantityMeasurement.java
+++ src/main/java/com/bridgelabz/service/QuantityMeasurement.java
@@ -1,9 +1,13 @@
 package com.bridgelabz.service;
 
+import com.bridgelabz.exception.QuantityMeasurementException;
+
 public class QuantityMeasurement {
 
     //Method for comparing two objects.
-    public boolean compare(UnitConverter unitConverter, UnitConverter unitConverter2) {
+    public boolean compare(UnitConverter unitConverter, UnitConverter unitConverter2) throws QuantityMeasurementException {
+        if (unitConverter == null || unitConverter2 == null)
+            throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.NULL_EXCEPTION, "Null Object");
         return (unitConverter.equals(unitConverter2)) ? true : false;
     }
 }
