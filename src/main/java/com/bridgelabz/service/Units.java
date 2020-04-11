db7ebfa1d433fc1440110541572d6472f712bb29
sandipk01 <sandipkengar01@gmail.com>
2020-04-11 11:38:43 +0530

    REFACTORED : Gallon milliliter litter units are added

--- src/main/java/com/bridgelabz/service/Units.java
+++ src/main/java/com/bridgelabz/service/Units.java
@@ -3,7 +3,9 @@ package com.bridgelabz.service;
 public enum Units {
 
     INCHES(UnitType.LENGTH,1), FEET(UnitType.LENGTH,12),
-    YARDS(UnitType.LENGTH, 36),CENTIMETER(UnitType.LENGTH, 0.4);
+    YARDS(UnitType.LENGTH, 36),CENTIMETER(UnitType.LENGTH, 0.4),
+    LITRE(UnitType.VOLUME, 1), GALLON(UnitType.VOLUME, 3.78),
+    MILLILITRE(UnitType.VOLUME,0.001);
 
     private double unitValue;
     private UnitType unitType;
